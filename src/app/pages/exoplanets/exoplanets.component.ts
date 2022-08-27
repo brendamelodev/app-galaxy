7adee7c881ce7317e86a9db13caaf713fc8ca5c5
brendamelodev <brendarebeka.melo@gmail.com>
2022-08-27 16:23:03 -0300

    add details with dialog

--- src/app/pages/exoplanets/exoplanets.component.ts
+++ src/app/pages/exoplanets/exoplanets.component.ts
@@ -1,8 +1,10 @@
+import { DetailsDialogComponent } from './../details-dialog/details-dialog.component';
 import { ExoplanetService } from './../../services/exoplanet.service';
 import { Exoplanet } from './../../models/exoplanets';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-exoplanets',
@@ -20,9 +22,11 @@ export class ExoplanetsComponent implements OnInit {
   paginator!: MatPaginator;
 
   private service: ExoplanetService;
+  private dialog: MatDialog;
 
-  constructor(service: ExoplanetService) {
+  constructor(service: ExoplanetService, dialog: MatDialog) {
     this.service = service;
+    this.dialog = dialog;
   }
 
   ngOnInit(): void {
@@ -41,5 +45,9 @@ export class ExoplanetsComponent implements OnInit {
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  openDetailsDialog(exoplanet: Exoplanet): void {
+    this.dialog.open(DetailsDialogComponent, { data: exoplanet, width: "400px" });
   }
 }
